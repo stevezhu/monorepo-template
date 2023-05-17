e91ed365abeb73fceaff9ee932909459a0f4eb30
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2023-05-17 00:04:22 -0400

    Setup example boilerplate

---
+++ apps/example/src/main.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const rootEl = document.getElementById('app');
+if (!rootEl) throw new Error('Failed to get root element');
+
+const root = createRoot(rootEl);
+root.render(<App />);
