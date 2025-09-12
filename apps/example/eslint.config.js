ba2c994eee61bd5d5cdbb00ced05295df1df4770
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-09-12 12:47:14 -0400

    Update deps

--- apps/example/eslint.config.js
+++
@@ -1,6 +0,0 @@
-// @ts-check
-
-import { defineConfig } from '@stzhu/eslint-config';
-import reactConfig from '@stzhu/eslint-config/react';
-
-export default defineConfig(...reactConfig);
--- eslint.config.js
+++
@@ -1,6 +0,0 @@
-// @ts-check
-
-import { defineConfig } from '@stzhu/eslint-config';
-import nodeConfig from '@stzhu/eslint-config/node';
-
-export default defineConfig(...nodeConfig);
