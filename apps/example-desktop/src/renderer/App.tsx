2b82f2939abf1338820d3bb98ba51f6f21bef41e
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-10-03 18:58:52 -0400

    Add react

---
+++ apps/example-desktop/src/renderer/App.tsx
@@ -0,0 +1,11 @@
+import { Counter } from '@workspace/ui';
+
+export function App() {
+  return (
+    <>
+      <h1>💖 Hello World!</h1>
+      <p>Welcome to your Electron application.</p>
+      <Counter />
+    </>
+  );
+}
--- apps/example-desktop/src/renderer/main.tsx
+++ apps/example-desktop/src/renderer/main.tsx
@@ -1,33 +1,16 @@
-/**
- * This file will automatically be loaded by vite and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.ts` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
+import './main.css';
 
-import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-console.log(
-  '👋 This message is being logged by "renderer.ts", included via Vite',
+import { App } from './App';
+
+const rootEl = document.getElementById('root');
+if (!rootEl) throw new Error('Failed to get root element');
+
+const root = createRoot(rootEl);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
