a51726c86ebb3237a34105bba4e298e76d4db1bf
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-10-03 17:51:07 -0400

    Support esm

--- apps/example-desktop/vite.main.config.ts
+++ apps/example-desktop/vite.main.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/main.ts',
+      formats: ['es'],
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
+    },
+  },
+});
